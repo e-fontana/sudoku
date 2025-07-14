66d871ae1b2dc9985d935b8101d579626a33cdd6
andreojr <atipicode.ec@gmail.com>
2025-07-14 15:04:34 -0300

    complete logic working (left test stopwatch and score)

--- SudokuMapCreator/sudoku.py
+++ SudokuMapCreator/sudoku.py
@@ -68,7 +68,7 @@ class Sudoku:
                     return False
         return True
 
-    def remove_cells(self, clues=70):
+    def remove_cells(self, clues=79):
         positions = [(i, j) for i in range(9) for j in range(9)]
         random.shuffle(positions)
 
