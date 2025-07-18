db2fb4c88ec99e284ba45b697834b6dd177499b5
andreojr <atipicode.ec@gmail.com>
2025-07-18 20:38:20 -0300

    Remove obsolete Quartus project file top.qws

--- modules/game/map_selector.v
+++ modules/game/map_selector.v
@@ -6,8 +6,8 @@ module map_selector (
     output reg [323:0] selected_map
 );
     // Wires para receber os dados de todos os mapas
-    wire [2429:0] visibilities_easy, visibilities_hard;
-    wire [4859:0] maps_easy, maps_hard;
+    wire [161:0] visibilities_easy, visibilities_hard;
+    wire [323:0] maps_easy, maps_hard;
 
     // Wire para receber o índice (0-7) do nosso novo gerador
     wire [2:0] map_index;
@@ -32,11 +32,11 @@ module map_selector (
             selected_map        <= 324'd0;
         end else begin
             if (difficulty) begin
-                selected_visibility <= visibilities_hard[(map_index * 162) +: 162];
-                selected_map        <= maps_hard[(map_index * 324) +: 324];
+                selected_visibility <= visibilities_hard;
+                selected_map        <= maps_hard;
             end else begin
-                selected_visibility <= visibilities_easy[(map_index * 162) +: 162];
-                selected_map        <= maps_easy[(map_index * 324) +: 324];
+                selected_visibility <= visibilities_easy;
+                selected_map        <= maps_easy;
             end
         end
     end
